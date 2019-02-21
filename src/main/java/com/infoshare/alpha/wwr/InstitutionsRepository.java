630073d89e734b2260ada75afcccdc7f84116482
pkowerzanow <piotr.kowerzanow@gmail.com>
2019-02-22 00:42:00 +0100

    JZ6AL-7: add getAllIntitutions

--- src/main/java/com/infoshare/alpha/wwr/InstitutionsRepository.java
+++ src/main/java/com/infoshare/alpha/wwr/InstitutionsRepository.java
@@ -6,18 +6,28 @@ import java.util.stream.Collectors;
 
 public class InstitutionsRepository {
 
+    private List<Institution> institutions = new ArrayList<>();
+
+    public InstitutionsRepository() {
+        this.institutions.add(new Institution("WWR1", new Address("Gdańsk", "Nowe Ogroady 23", "+48 564 123 123")));
+        this.institutions.add(new Institution("WWR2", new Address("Gdańsk", "Al. Gruwaldzka 223", "+48 564 123 555")));
+        this.institutions.add(new Institution("WWR3", new Address("Sopot", "Zwyciestwa 17", "+48 754 123 123")));
+        this.institutions.add(new Institution("WWR4", new Address("Gdynia", "Kolejowa 23", "+48 112 123 123")));
+        this.institutions.add(new Institution("WWR5", new Address("Gdynia", "Swietojanska 110", "+48 456 123 123")));
+    }
+
     public List<Institution> findInstitutionsByCity(String city) {
 
-        List<Institution> institutions = new ArrayList<>();
-        institutions.add(new Institution("WWR1", new Address("Gdańsk", "Nowe Ogroady 23", "+48 564 123 123")));
-        institutions.add(new Institution("WWR2", new Address("Gdańsk", "Al. Gruwaldzka 223", "+48 564 123 555")));
-        institutions.add(new Institution("WWR3", new Address("Sopot", "Zwyciestwa 17", "+48 754 123 123")));
-        institutions.add(new Institution("WWR4", new Address("Gdynia", "Kolejowa 23", "+48 112 123 123")));
-        institutions.add(new Institution("WWR5", new Address("Gdynia", "Swietojanska 110", "+48 456 123 123")));
-
-        return institutions.
+        return this.institutions.
                 stream().
                 filter(s->city.equals(s.getAddress().getCity()))
                 .collect(Collectors.toList());
     }
+
+    public List<Institution> getAllIntitutions() {
+
+        return this.institutions;
+    }
+    
+    
 }
