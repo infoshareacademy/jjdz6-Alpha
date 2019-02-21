62b8ddb10fff9414a27ce9a141ea3d5af8c3c469
pkowerzanow <piotr.kowerzanow@gmail.com>
2019-02-21 23:54:51 +0100

    JZ6AL-7: add class InstitutionRepository and findInstitutionsByCity func

--- src/main/java/com/infoshare/alpha/App.java
+++ src/main/java/com/infoshare/alpha/App.java
@@ -7,7 +7,8 @@ import com.infoshare.alpha.wwr.Pesel;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 /**
  * Hello world!
@@ -29,18 +30,26 @@ public class App
         Patient patient = new Patient("Jan", "Nowak", pesel , address );
         
         List<Institution> institutions = new ArrayList<Institution>();   
-        
+
         institutions.add(new Institution("WWR1", new Address("Gdańsk", "Nowe Ogroady 23", "+48 564 123 123")));
         institutions.add(new Institution("WWR2", new Address("Gdańsk", "Al. Gruwaldzka 223", "+48 564 123 555")));
         institutions.add(new Institution("WWR3", new Address("Sopot", "Zwyciestwa 17", "+48 754 123 123")));
         institutions.add(new Institution("WWR4", new Address("Gdynia", "Kolejowa 23", "+48 112 123 123")));
         institutions.add(new Institution("WWR5", new Address("Gdynia", "Swietojanska 110", "+48 456 123 123")));
-        
-        
-        Optional<Institution> inst = institutions.stream().filter(i->"Gdańsk".equals(i.getAddress().getCity())).findAny();
-        
 
-        System.out.println(inst.toString());
+
+        String filter = "Gdańsk";
+
+        List<Institution> filteredInstitutions = institutions.stream().filter(s->filter.equals(s.getAddress().getCity())).collect(Collectors.toList());
+
+        for (Institution i : filteredInstitutions) {
+            System.out.println(i.toString());
+        }
+        //institutions.stream().filter(s->"Gdańsk".equals(s.getAddress().getCity())).findAny();
+
+
+//        Optional<Institution> inst = institutions.stream().filter(i->"Gdańsk".equals(i.getAddress().getCity())).findAny();
+//        System.out.println(inst.toString());
         
  
         
