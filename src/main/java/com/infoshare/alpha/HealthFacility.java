56577be02b1030d781696b110e19441f0531d221
Dorota Miler <dorota.miler@outlook.com>
2019-03-04 17:41:07 +0100

    creation of 2 sub-menus for seperate classes: Patient.java and HealthFacility.java; cleaner version of functins for client's friendlier use

---
+++ src/main/java/com/infoshare/alpha/HealthFacility.java
@@ -0,0 +1,16 @@
+package com.infoshare.alpha;
+
+public class HealthFacility {
+    public static void healthFacilitySubMenu() {
+        System.out.println();
+        System.out.println("                   HEALTH FACILITY subMENU             ");
+        System.out.println("     *   Please choose an adequate option:            *");
+        System.out.println("     **************************************************");
+        System.out.println("     1. Find the nearest patient's health facility     ");
+        System.out.println("     2. Enter health facility's information            ");
+        System.out.println("     3. Edit health facility's information             ");
+
+        System.out.println();
+        System.out.println("     Enter your selection: _                            ");
+    }
+}
---
+++ src/main/java/com/infoshare/alpha/Patient.java
@@ -0,0 +1,17 @@
+package com.infoshare.alpha;
+
+public class Patient {
+    public static void patientSubMenu() {
+        System.out.println();
+        System.out.println("                   bHEALTH FACILITY subMENU            ");
+        System.out.println("     *   Please choose an adequate option:            *");
+        System.out.println("     **************************************************");
+        System.out.println("     1. Enter patient's information                    ");
+        System.out.println("     2. Edit patient's information                     ");
+        System.out.println("     3. Delete patient's information                   ");
+        System.out.println("     4. Display all patients' information              ");
+        System.out.println("     5. Display given patient's information            ");
+        System.out.println();
+        System.out.println("     Enter your selection: _                           ");
+    }
+}
