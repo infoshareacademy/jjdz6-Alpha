5a7899be9fcd28957530220bfe891f4713bff69a
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-04-13 20:58:35 +0200

    add package servlet and classes CustomizedServlet, FacilityServlet and PatientServlet

---
+++ src/main/java/com/infoshare/alpha/wwr/servlet/PatientServlet.java
@@ -0,0 +1,36 @@
+package com.infoshare.alpha.wwr.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/patients")
+public class PatientServlet extends CustomizedServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
