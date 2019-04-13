7f2e236fd275a718faa007ac8228b529bf16eded
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-04-13 21:33:31 +0200

    add servlet PatientQueryServlet

--- src/main/java/com/infoshare/alpha/wwr/servlet/CustomizedServlet.java
+++ src/main/java/com/infoshare/alpha/wwr/servlet/CustomizedServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 public abstract class CustomizedServlet extends HttpServlet {
 
@@ -17,5 +18,13 @@ public abstract class CustomizedServlet extends HttpServlet {
         }
     }
 
-    protected abstract void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException;
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String protocol = req.getProtocol();
+        String msg = ResourceBundle.getBundle("javax.servlet.http.LocalStrings").getString("http.method_patch_not_supported");
+        if (protocol.endsWith("1.1")) {
+            resp.sendError(405, msg);
+        } else {
+            resp.sendError(400, msg);
+        }
+    }
 }
