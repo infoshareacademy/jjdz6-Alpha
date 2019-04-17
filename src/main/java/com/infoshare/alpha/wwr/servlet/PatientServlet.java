8c5dfb0a68889735a5eb806ceba62411ebf25e1f
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-04-17 21:04:49 +0200

    delete FacilityServlet from package facilities.servlet and modify annotations in servlets from package wwr.servlet

--- src/main/java/com/infoshare/alpha/wwr/servlet/PatientServlet.java
+++ src/main/java/com/infoshare/alpha/wwr/servlet/PatientServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/patients")
+@WebServlet(name = "PatientServlet", urlPatterns = {"/patient"})
 public class PatientServlet extends BaseWwrServlet {
 
     @Override
