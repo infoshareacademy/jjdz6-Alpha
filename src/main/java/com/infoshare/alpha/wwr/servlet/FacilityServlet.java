71a7b2555bd3648c5dedf07903e460c2ee6b6fde
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-04-17 20:25:21 +0200

    refactor servlets as discussed:
    renamed:    CustomizedServlet -> BaseWwrServlet
    deleted:    PatientQueryServlet

--- src/main/java/com/infoshare/alpha/wwr/servlet/FacilityServlet.java
+++ src/main/java/com/infoshare/alpha/wwr/servlet/FacilityServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/facilities")
-public class FacilityServlet extends CustomizedServlet {
+public class FacilityServlet extends BaseWwrServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
