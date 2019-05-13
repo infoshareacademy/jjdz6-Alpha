4994c2a0075d255adab31114027717e2362d040d
Dorota Miler <dorota.miler@outlook.com>
2019-05-14 01:20:16 +0200

    modification of Facility.java

--- src/main/java/com/infoshare/alpha/wwr/domain/facilities/entity/Facility.java
+++ src/main/java/com/infoshare/alpha/wwr/domain/facilities/entity/Facility.java
@@ -12,22 +12,20 @@ import java.util.UUID;
 public class Facility {
 
     private UUID id;
-
-	private String name;
-    
-	private Address address;
-
-	private List<Service> services = new ArrayList<>();
+    private String name;
+    private Address address;
+    private Boolean NFZ;
+    private List<Service> services = new ArrayList<>();
 
 
-	public Facility(String name, Address address, List<Service> services) {
+    public Facility(String name, Address address, List<Service> services) {
         this.address = address;
         this.name = name;
         this.services = services;
         this.id = UUID.randomUUID();
     }
 
-    public Facility(String name, Address address) {
+    public Facility() {
         this(name, address, UUID.randomUUID());
     }
 
@@ -37,35 +35,53 @@ public class Facility {
         this.id = id;
     }
 
-    public Facility(String name, Address address, UUID id, List<Service> services) {
+    public Facility(String name, Address address, UUID id, Boolean NFZfacility, <Service>services) {
         this.address = address;
         this.name = name;
         this.id = id;
+        this.NFZfacility = NFZfacility;
         this.services = services;
     }
 
     public String getName() {
- 
-    	return name;
+        return name;
     }
-    
-    public Address getAddress() {
 
-    	return address;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
     public UUID getId() {
- 
-    	return id;
+        return id;
     }
 
     public List<Service> getServices() {
         return this.services;
     }
 
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
+
+    public Boolean getNFZ() {
+        return NFZfacility;
+    }
+
+    public void setNFZ(Boolean NFZfacility) {
+        this.NFZfacility = NFZfacility;
+    }
+
     @Override
     public String toString() {
-    	return "Id: " + this.id.toString() + " Name : " + this.name + this.address.toString() + " Facilities : " + Services.fromList(this.services) ;
+        return "Id: " + this.id.toString() + " Name : " + this.name + this.address.toString() + " Facilities : " + Services.fromList(this.services);
     }
 
     @Override
@@ -75,11 +91,12 @@ public class Facility {
         Facility facility = (Facility) o;
         return Objects.equals(getName(), facility.getName()) &&
                 Objects.equals(getAddress(), facility.getAddress()) &&
-                Objects.equals(getServices(), facility.getServices());
+                Objects.equals(getServices(), facility.getServices()) &&
+                Objects.equals(getNFZ(), facility.getNFZ());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName(), getAddress(), getServices());
+        return Objects.hash(getName(), getAddress(), getServices(), getNFZ());
     }
 }
