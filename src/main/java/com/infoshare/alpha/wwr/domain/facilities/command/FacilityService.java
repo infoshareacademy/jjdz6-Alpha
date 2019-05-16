b59becc392f015eb6444aa330979eec8b6072fa0
Dorota Miler <dorota.miler@outlook.com>
2019-05-17 00:51:54 +0200

    FacilityService.java addition connected with doPost() in FacilityServlet - modification in lines with add-facility.html

---
+++ src/main/java/com/infoshare/alpha/wwr/domain/facilities/command/FacilityService.java
@@ -0,0 +1,18 @@
+package com.infoshare.alpha.wwr.domain.facilities.command;
+
+import com.infoshare.alpha.wwr.domain.facilities.entity.Facilities;
+import com.infoshare.alpha.wwr.domain.facilities.entity.Facility;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class FacilityService {
+
+    @EJB
+    Facilities facilities;
+
+    public void saveFacility(Facility facility) {
+        facilities.add(facility);
+    }
+}
--- src/main/java/com/infoshare/alpha/wwr/servlet/FacilityServlet.java
+++ src/main/java/com/infoshare/alpha/wwr/servlet/FacilityServlet.java
@@ -1,5 +1,9 @@
 package com.infoshare.alpha.wwr.servlet;
 
+import com.infoshare.alpha.wwr.common.Address;
+import com.infoshare.alpha.wwr.domain.facilities.command.FacilityService;
+import com.infoshare.alpha.wwr.domain.facilities.entity.Facility;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +13,9 @@ import java.io.IOException;
 @WebServlet(name = "FacilityServlet", urlPatterns = {"/facility"})
 public class FacilityServlet extends BaseWwrServlet {
 
+    @Inject
+    FacilityService facilityService;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -17,6 +24,18 @@ public class FacilityServlet extends BaseWwrServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        Facility facility = new Facility();
+        Address address = new Address();
+
+        facility.setName(String.valueOf(req.getParameter("name")));
+        address.setStreet(String.valueOf(req.getParameter("street")));
+        address.setCity(String.valueOf(req.getParameter("city")));
+        address.setPhone(String.valueOf(req.getParameter("phone")));
+
+
+        facilityService.saveFacility(facility);
+
+        resp.getWriter().println("Facility added successfully");
     }
 
     @Override
