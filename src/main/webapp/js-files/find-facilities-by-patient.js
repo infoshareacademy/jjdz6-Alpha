43d450359c62bd0e5f7ef434bbc3cadc9789a3ef
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-05-24 21:35:51 +0200

    add function to toggle facility details in file find-facilities-by-patient.js

--- src/main/webapp/js-files/find-facilities-by-patient.js
+++ src/main/webapp/js-files/find-facilities-by-patient.js
@@ -15,4 +15,22 @@ $(document).ready(function () {
             request.send();
         }catch(e){alert("Unable to connect to server");}
     }
-}
+});
+
+$(document).ready(function () {
+    $(".facility").on("click", function () {
+        $(".facility-details").toggle();
+    })
+});
+
+// $(document).ready(function () {
+//         var facilities = document.getElementsByClassName("facility");
+//         for (var i = 0; i < facilities.length; i++) {
+//             var index = i;
+//             facilities[i].addEventListener("click", function (evt) {
+//                 var details = document.getElementsByClassName("facility-details").item(index);
+//                 $(details).toggle();
+//             })
+//         }
+//     }
+// );
