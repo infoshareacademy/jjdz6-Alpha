114aefcfc56d50c5dfedd18533a35da410ded566
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-05-30 00:05:00 +0200

    turn toggle off for children of class facility

--- src/main/webapp/js-files/find-facilities-by-patient.js
+++ src/main/webapp/js-files/find-facilities-by-patient.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    // TODO wyłączyć toggle przy clicku na class facility details
     $(".facility").on("click", function () {
         $(this).find(".facility-details").toggle();
+    }).on("click", "ul", function (e) {
+        e.stopPropagation();
     })
 });
