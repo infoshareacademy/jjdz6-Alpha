a5d3a1708d9dc432c82ba10088396c85ae199717
Piotr Kowerzanow <pkowerzanow@getresponse.com>
2019-06-06 16:47:53 +0200

    jz6al-20: add ut for delete method

--- src/test/java/com/infoshare/alpha/wwr/domain/facilities/FacilitiesServiceTest.java
+++ src/test/java/com/infoshare/alpha/wwr/domain/facilities/FacilitiesServiceTest.java
@@ -3,6 +3,7 @@ package com.infoshare.alpha.wwr.domain.facilities;
 import com.infoshare.alpha.wwr.common.Address;
 import com.infoshare.alpha.wwr.common.Service;
 import com.infoshare.alpha.wwr.domain.facilities.command.FacilityAddCommand;
+import com.infoshare.alpha.wwr.domain.facilities.command.FacilityDeleteCommand;
 import com.infoshare.alpha.wwr.domain.facilities.command.FacilityEditCommand;
 import com.infoshare.alpha.wwr.domain.facilities.common.FacilitiesException;
 import com.infoshare.alpha.wwr.domain.facilities.entity.Facilities;
@@ -111,6 +112,72 @@ class FacilitiesServiceTest {
     }
 
     @Test
+    @DisplayName("Should delete facility")
+    void shouldDeleteFacility() {
+
+        // given
+
+        List<Service> services = getFacilityServices();
+        Facility facility1 = new Facility(
+                1,
+                "facility-1",
+                new Address("Gdańsk", "Kolejowa 23", "+48 123 123 123"),
+                services
+        );
+
+        List<Facility> facilitiesList = new ArrayList<>();
+        facilitiesList.add(facility1);
+        Facilities facilities = Mockito.mock(Facilities.class);
+
+        // when
+
+        Mockito.when(facilities.getFacilities()).thenReturn(facilitiesList);
+        Mockito.when(facilitiesReadModelDbRepository.getAll()).thenReturn(facilities);
+
+        FacilityDeleteCommand facilityDeleteCommand = new FacilityDeleteCommand(facility1);
+
+        try {
+            testObj.delete(facilityDeleteCommand);
+        } catch (FacilitiesException e) {
+            e.printStackTrace();
+        }
+
+        // then
+
+        Mockito.verify(facilitiesDbRepository, Mockito.times(1)).add(facilities);
+    }
+
+    @Test
+    @DisplayName("Should not delete facility while not exist.")
+    void shouldNotDeleteFacilityWhileNotExist() {
+
+        // given
+
+        List<Service> services = getFacilityServices();
+        Facility facility1 = new Facility(
+                1,
+                "facility-1",
+                new Address("Gdańsk", "Kolejowa 23", "+48 123 123 123"),
+                services
+        );
+
+        List<Facility> facilitiesList = new ArrayList<>();
+        Facilities facilities = Mockito.mock(Facilities.class);
+
+        FacilityDeleteCommand facilityDeleteCommand = new FacilityDeleteCommand(facility1);
+
+        // when/then
+
+        Mockito.when(facilities.getFacilities()).thenReturn(facilitiesList);
+        Mockito.when(facilitiesReadModelDbRepository.getAll()).thenReturn(facilities);
+
+        assertThatThrownBy(() -> testObj.delete(facilityDeleteCommand))
+                .isInstanceOf(FacilitiesException.class)
+                .hasMessage("Facility " + facility1.getName() + " not found ");
+
+    }
+
+    @Test
     @DisplayName("Should edit facility")
     void shouldEdit() {
 
@@ -155,14 +222,6 @@ class FacilitiesServiceTest {
         Mockito.verify(facilities, Mockito.times(5)).getFacilities();
         Mockito.verify(facilitiesReadModelDbRepository, Mockito.times(1)).getAll();
         Mockito.verify(facilitiesDbRepository, Mockito.times(1)).add(facilities);
-    }
-
-    private static List<Service> getFacilityServices() {
-        List<Service> services = new ArrayList<>();
-        services.add(new Service("wspomaganie-1"));
-        services.add(new Service("wspomaganie-2"));
-        services.add(new Service("wspomaganie-3"));
-        return services;
     }
 
     @Test
@@ -240,6 +299,14 @@ class FacilitiesServiceTest {
         assertThatThrownBy(() -> testObj.edit(facilityEditCommand))
                 .isInstanceOf(FacilitiesException.class)
                 .hasMessage("Facility " + editedFacility.getName() + " already exists ");
+    }
+
+    private static List<Service> getFacilityServices() {
+        List<Service> services = new ArrayList<>();
+        services.add(new Service("wspomaganie-1"));
+        services.add(new Service("wspomaganie-2"));
+        services.add(new Service("wspomaganie-3"));
+        return services;
     }
 
 }
