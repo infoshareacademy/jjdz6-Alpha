773e11f87395c97c4d5bdcdd1541c85874ebcf3a
Piotr Kowerzanow <pkowerzanow@getresponse.com>
2019-06-07 16:42:51 +0200

    jz6al-20: update ut for Pesel class

---
+++ src/test/java/com/infoshare/alpha/wwr/common/PeselTest.java
@@ -0,0 +1,40 @@
+package com.infoshare.alpha.wwr.common;
+
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class PeselTest {
+
+    @Test
+    @DisplayName("Should instantiate pesel")
+    void shouldInstantiate() throws PeselException {
+
+        // given
+
+
+        String peselNumber = "90121212345";
+
+        // when/then
+
+        IsInstanceOf.instanceOf(Pesel.class).matches(new Pesel(peselNumber));
+    }
+
+    @Test
+    @DisplayName("Should not instantiate when pesel invalid")
+    void shouldNotInstantiateWhenPeselInvalid() {
+
+        // given
+
+        String invlidPeselNumber = "1234";
+
+        // when/then
+
+        assertThatThrownBy(() -> new Pesel(invlidPeselNumber))
+                .isInstanceOf(PeselException.class)
+                .hasMessage("Pesel not valid");
+    }
+}
