5b990e04566ae8f9adc75f0a75431f835694b010
Dorota Miler <dorota.miler@outlook.com>
2019-06-11 23:29:00 +0200

    Facility.java accomodation- resolution of conflicts

--- src/main/java/com/infoshare/alpha/wwr/domain/facilities/entity/Facility.java
+++ src/main/java/com/infoshare/alpha/wwr/domain/facilities/entity/Facility.java
@@ -34,11 +34,7 @@ public class Facility {
         this.id = id;
     }
 
-<<<<<<< HEAD
-    public Facility(String name, Address address, UUID id, Boolean isNfz, List<Service> services) {
-=======
-    public Facility(String name, Address address, int id, List<Service> services) {
->>>>>>> develop
+    public Facility(String name, Address address, int id, Boolean isNfz, List<Service> services) {
         this.address = address;
         this.name = name;
         this.id = id;
@@ -46,18 +42,19 @@ public class Facility {
         this.services = services;
     }
 
-<<<<<<< HEAD
     public Facility(String name, Address address, Boolean isNfz) {
         this.address = address;
         this.name = name;
         this.isNfz = isNfz;
     }
-=======
-    public Facility(String id, String name, Address address, List<Service> services) {
+
+    public Facility(int id, String name, Address address, List<Service> services) {
+        this.address = address;
+        this.name = name;
+        this.id = id;
+        this.services = services;
     }
 
-    public String getName() {
->>>>>>> develop
 
     public String getName() {
         return name;
@@ -67,12 +64,7 @@ public class Facility {
         return address;
     }
 
-<<<<<<< HEAD
-    public UUID getId() {
-=======
     public int getId() {
-
->>>>>>> develop
         return id;
     }
 
@@ -90,29 +82,20 @@ public class Facility {
 
     @Override
     public String toString() {
-<<<<<<< HEAD
-        return " Id : " + this.id.toString() +
+        return " Id : " + this.id +
                 " Name : " + this.name + this.address.toString() +
                 " Nfz : " + valueOf(isNfz) +
                 " Services : " + Services.fromList(this.services);
-=======
-        return "Id: " + this.id + " Name : " + this.name + this.address.toString() + " Facilities : " + Services.fromList(this.services);
->>>>>>> develop
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Facility)) return false;
-        Facility facility = (Facility) o;
-<<<<<<< HEAD
         return Objects.equals(getName(), facility.getName()) &&
                 Objects.equals(getAddress(), facility.getAddress()) &&
                 Objects.equals(isNfz(), facility.isNfz()) &&
                 Objects.equals(getServices(), facility.getServices());
-=======
-        return Objects.equals(getName(), facility.getName()) && Objects.equals(getAddress(), facility.getAddress()) && Objects.equals(getServices(), facility.getServices());
->>>>>>> develop
     }
 
     @Override
@@ -120,3 +103,4 @@ public class Facility {
         return Objects.hash(getName(), getAddress(), isNfz(), getServices());
     }
 }
+
