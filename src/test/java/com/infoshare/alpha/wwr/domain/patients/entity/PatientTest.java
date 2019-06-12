c63b936f4033bdf39dfc2a2c87db7425fa828d63
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-06-12 21:29:16 +0200

    change test methods in class PatientTest

--- src/test/java/com/infoshare/alpha/wwr/domain/patients/entity/PatientTest.java
+++ src/test/java/com/infoshare/alpha/wwr/domain/patients/entity/PatientTest.java
@@ -24,34 +24,21 @@ class PatientTest {
     }
 
     @Test
-    @DisplayName("Should not return 0 when surname is different")
-    void comparePatientsWithDifferentSurname() {
+    @DisplayName("Should return 0")
+    void shouldEquals() {
         // given
-        Patient givenPatient = new Patient("TestName", "GivenSurname", givenPesel, new Address("TestCity", "TestStreet", "123 456 789", 80000), new Parent("TestParentName", "TestParentSurname"));
+        Patient givenPatient = new Patient("TestName", "TestSurname", givenPesel, new Address("TestCity", "TestStreet", "123 456 789", 80000), new Parent("TestParentName", "TestParentSurname"));
 
         // when
         int result = testObj.compareTo(givenPatient);
 
         // then
-        assertThat(result).isNotZero();
+        assertThat(result).isZero();
     }
 
     @Test
-    @DisplayName("Should not return 0 when name is different")
-    void comparePatientsWithDifferentName() {
-        // given
-        Patient givenPatient = new Patient("GivenName", "TestSurname", givenPesel, new Address("TestCity", "TestStreet", "123 456 789", 80000), new Parent("TestParentName", "TestParentSurname"));
-
-        // when
-        int result = testObj.compareTo(givenPatient);
-
-        // then
-        assertThat(result).isNotZero();
-    }
-
-    @Test
-    @DisplayName("Should not return 0 when Pesel is different")
-    void comparePatientsWithDifferentPesel() {
+    @DisplayName("Should not return 0")
+    void shouldNotEquals() {
         // given
         Pesel givenPesel2 = null;
         try {
@@ -66,21 +53,5 @@ class PatientTest {
 
         // then
         assertThat(result).isNotZero();
-    }
-
-    @Test
-    @DisplayName("Should return 0 when Surname, Name and Pesel are equal")
-    void comparePatientsWithSameSurnameNameAndPesel() {
-        // given
-        Patient givenPatient = new Patient("TestName", "TestSurname", givenPesel, new Address("GivenCity", "GivenStreet", "987 654 321", 80200), new Parent("GivenParentName", "GivenParentSurname"));
-
-        // when
-        int result = testObj.compareTo(givenPatient);
-
-        // then
-        assertThat(testObj.getName()).isEqualTo(givenPatient.getName());
-        assertThat(testObj.getSurname()).isEqualTo(givenPatient.getSurname());
-        assertThat(testObj.getPesel()).isEqualTo(givenPatient.getPesel());
-        assertThat(result).isEqualTo(0);
     }
 }
