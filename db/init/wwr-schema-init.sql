a5b9edd87b738fd57b97a7fa1ee3187f286b4b8c
Piotr Kowerzanow <pkowerzanow@getresponse.com>
2019-06-19 00:13:43 +0200

    docker: wip : update init sqls

--- db/init/wwr-schema-init.sql
+++ db/init/wwr-schema-init.sql
@@ -1,15 +1,15 @@
 -- initialize wwr db schema , init sample data
 
 -- entity tables
+CREATE TABLE patients (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
 create table facilities (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL ,
   is_nfz BOOLEAN NOT NULL
-);
-
-create table patients (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
 );
 
 create table services (
@@ -26,41 +26,37 @@ create table addresses (
 );
 
 -- relational tables
-create table facilities_addresses (
+ create table facilities_addresses (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     facility_id INT NOT NULL,
     address_id INT NOT NULL,
-    PRIMARY KEY (id),
     CONSTRAINT fk_facilities_addresses_facility_id FOREIGN KEY (facility_id) REFERENCES facilities(id),
     CONSTRAINT fk_facilities_addresses_address_id FOREIGN KEY (address_id) REFERENCES addresses(id)
-);
+ );
 
-create table patients_addresses (
+ create table patients_addresses (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     patient_id INT NOT NULL,
     address_id INT NOT NULL,
-    PRIMARY KEY (id),
     CONSTRAINT fk_patients_addresses_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id),
     CONSTRAINT fk_patients_addresses_address_id FOREIGN KEY (address_id) REFERENCES addresses(id)
-);
+ );
 
-create table facilities_services (
+ create table facilities_services (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     facility_id INT NOT NULL,
     service_id INT NOT NULL,
-    PRIMARY KEY (id),
     CONSTRAINT fk_facilities_services_facility_id FOREIGN KEY (facility_id) REFERENCES facilities(id),
     CONSTRAINT fk_facilities_services_service_id FOREIGN KEY (service_id) REFERENCES services(id)
-);
+ );
 
-create table patients_services (
+ create table patients_services (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     patient_id INT NOT NULL,
     service_id INT NOT NULL,
-    PRIMARY KEY (id),
     CONSTRAINT fk_patients_services_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id),
     CONSTRAINT fk_patients_services_service_id FOREIGN KEY (service_id) REFERENCES services(id)
-);
+ );
 
 
 
