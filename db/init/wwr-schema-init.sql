c3aeebdd1abab4fc179846256e6d409b0a6a85a6
pkowerzanow <piotr.kowerzanow@gmail.com>
2019-07-23 01:18:14 +0200

    jz6al-39: add fcilities data to init sql

--- db/init/wwr-schema-init.sql
+++ db/init/wwr-schema-init.sql
@@ -30,11 +30,21 @@ create table addresses (
     postalCode INT NOT NULL
 );
 -- patients addresses
-insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Pilotów 23", "556 454 234", 80460);
-insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Miałki Szlak 12", "556 111 222", 81560);
-insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Warneńska 80", "756 121 232", 80210);
-insert into addresses (city, street, phone, postalCode) VALUES ("Sopot", "Warneńska 80", "856 422 226", 80210);
-insert into addresses (city, street, phone, postalCode) VALUES ("Gdynia", "Polska 11", "556 343 442", 80210);
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Pilotów 23", "556 454 234", 80460); -- 1
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Miałki Szlak 12", "556 111 222", 81560); -- 2
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Warneńska 80", "756 121 232", 80210); -- 3
+insert into addresses (city, street, phone, postalCode) VALUES ("Sopot", "Warneńska 80", "856 422 226", 80210); -- 4
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdynia", "Polska 11", "556 343 442", 80210); -- 5
+
+-- facilities addresses
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdynia", "Augustyna Necla 11-13", "+48 58 621 74 40", 81377); -- 6
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Kartuska 214", "+48 564 123 555", 80122); -- 7
+insert into addresses (city, street, phone, postalCode) VALUES ("Sopot", "Zwyciestwa 17", "+48 754 123 123", 81704); -- 8
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdynia", "Kolejowa 23", "+48 112 123 123", 81303); -- 9
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk", "Jagiellońska 11", "+48 508 186 021", 81368); -- 10
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdańsk-Oliwa", "Matemblewska 42", "+48 602 491 421", 80830); -- 11
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdynia", "Harcerska 4", "+48 58 622 07 48", 81425); -- 12
+insert into addresses (city, street, phone, postalCode) VALUES ("Gdynia", "Portowa 1", "+48 112 234 123", 81339); -- 13
 -- ---------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE parents (
@@ -68,30 +78,61 @@ insert into patients (name, surname, pesel_id, parent_id, address_id) VALUES ("�
 insert into patients (name, surname, pesel_id, parent_id, address_id) VALUES ("Magda", "Jakowska", 2, 2, 2); -- id =2
 insert into patients (name, surname, pesel_id, parent_id, address_id) VALUES ("Adam", "Kowalski", 3, 3, 3); -- id =3
 insert into patients (name, surname, pesel_id, parent_id, address_id) VALUES ("Kornelia", "Dorocińska", 4, 4, 4); -- id =4
-insert into patients (name, surname, pesel_id, parent_id, address_id) VALUES ("Mieszko", "Kot", 5, 5, 5); -- id =5
+insert into patients (name, surname, pesel_id, parent_id, address_id) VALUES ("Mieszko", "Kot", 5, 5, 5);  -- id =5
 
 -- ---------------------------------------------------------------------------------------------------------------------
 
 create table facilities (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL ,
-  is_nfz BOOLEAN NOT NULL
+  is_nfz BOOLEAN NOT NULL,
+  address_id INT NOT NULL,
+  CONSTRAINT fk_facilities_address_id FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
 
+insert into facilities (name, is_nfz, address_id) values ("Ośrodek Wczesnej Interwencji - filia nr 1 Niepubliczny Zakład Opieki Zdrowotnej",true,6);
+insert into facilities (name, is_nfz, address_id) values ("Wyspa",false,7);
+insert into facilities (name, is_nfz, address_id) values ("Poradnia psychologiczno-pedagogiczna, filia nr 6",true,8);
+insert into facilities (name, is_nfz, address_id) values ("Poradnia psychologiczno-pedagogiczna, filia nr 7",true,9);
+insert into facilities (name, is_nfz, address_id) values ("Ośrodek Wczesnej Interwencji i Wspomagania Rozwoju",false,10);
+insert into facilities (name, is_nfz, address_id) values ("ODiTiS",true,11);
+insert into facilities (name, is_nfz, address_id) values ("Centrum Rehabilitacji Ośrodek Wczesnej Interwencji w Gdyni",true,12);
+insert into facilities (name, is_nfz, address_id) values ("Poradnia psychologiczno-pedagogiczna, filia nr 4",true,13);
+
+-- ---------------------------------------------------------------------------------------------------------------------
 create table services (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
 
+insert into services (name) values("Metoda Ruchu Rozwijającego W.Sherborne."); -- 1
+insert into services (name) values("Metoda Dobrego Startu (MDS) M.Bogdanowicz."); -- 2
+insert into services (name) values("Dziecięca matematyka E.Gruszczyk- Kolczyńskiej."); -- 3
+insert into services (name) values("Metoda pedagogiki zabawy."); -- 4
+insert into services (name) values("Elementy Metody M.Montessori."); -- 5
+insert into services (name) values("Metoda kinezjologii edukacyjnej Paula Dennisona."); -- 6
+insert into services (name) values("Elementy Metody zabawy niedyrektywnej V. Axline."); -- 7
+insert into services (name) values("Elementy Metody M.Frostig i D.Horne. "); -- 8
+insert into services (name) values("Gry i zabawy ogólnorozwojowe."); -- 9
+insert into services (name) values("Zajęcia w Sali Doświadczania Świata."); -- 10
+insert into services (name) values("Zabawy materiałami nieustrukturowanymi."); -- 11
+insert into services (name) values("Mandala."); -- 12
+insert into services (name) values("rehabilitacja"); -- 13
+insert into services (name) values("terapia SI"); -- 14
+insert into services (name) values("pedagog specjalny"); -- 15
+insert into services (name) values("psycholog specialny"); -- 16
+insert into services (name) values("neurologopeda"); -- 17
+insert into services (name) values("neurolog"); -- 18
+
 
 -- relational tables
- create table facilities_addresses (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    facility_id INT NOT NULL,
-    address_id INT NOT NULL,
-    CONSTRAINT fk_facilities_addresses_facility_id FOREIGN KEY (facility_id) REFERENCES facilities(id),
-    CONSTRAINT fk_facilities_addresses_address_id FOREIGN KEY (address_id) REFERENCES addresses(id)
- );
+-- create table facilities_addresses (
+--    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+--    facility_id INT NOT NULL,
+--    address_id INT NOT NULL,
+--    CONSTRAINT fk_facilities_addresses_facility_id FOREIGN KEY (facility_id) REFERENCES facilities(id),
+--    CONSTRAINT fk_facilities_addresses_address_id FOREIGN KEY (address_id) REFERENCES addresses(id)
+-- );
 
 -- to delete
 -- create table patients_addresses (
@@ -101,7 +142,7 @@ create table services (
 --    CONSTRAINT fk_patients_addresses_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id),
 --    CONSTRAINT fk_patients_addresses_address_id FOREIGN KEY (address_id) REFERENCES addresses(id)
 -- );
-
+-- ---------------------------------------------------------------------------------------------------------------------
  create table facilities_services (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     facility_id INT NOT NULL,
@@ -110,6 +151,37 @@ create table services (
     CONSTRAINT fk_facilities_services_service_id FOREIGN KEY (service_id) REFERENCES services(id)
  );
 
+ insert into facilities_services (facility_id, service_id) values(1,13);
+ insert into facilities_services (facility_id, service_id) values(1,14);
+ insert into facilities_services (facility_id, service_id) values(1,15);
+ insert into facilities_services (facility_id, service_id) values(1,16);
+ insert into facilities_services (facility_id, service_id) values(1,17);
+
+ insert into facilities_services (facility_id, service_id) values(2,13);
+ insert into facilities_services (facility_id, service_id) values(2,17);
+
+ insert into facilities_services (facility_id, service_id) values(3,15);
+ insert into facilities_services (facility_id, service_id) values(3,18);
+
+ insert into facilities_services (facility_id, service_id) values(4,13);
+ insert into facilities_services (facility_id, service_id) values(4,17);
+
+ insert into facilities_services (facility_id, service_id) values(5,15);
+ insert into facilities_services (facility_id, service_id) values(5,16);
+
+ insert into facilities_services (facility_id, service_id) values(6,14);
+ insert into facilities_services (facility_id, service_id) values(6,15);
+
+ insert into facilities_services (facility_id, service_id) values(7,13);
+ insert into facilities_services (facility_id, service_id) values(7,14);
+ insert into facilities_services (facility_id, service_id) values(7,15);
+ insert into facilities_services (facility_id, service_id) values(7,16);
+ insert into facilities_services (facility_id, service_id) values(7,17);
+
+ insert into facilities_services (facility_id, service_id) values(8,14);
+ insert into facilities_services (facility_id, service_id) values(8,15);
+
+-- ---------------------------------------------------------------------------------------------------------------------
  create table patients_services (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     patient_id INT NOT NULL,
