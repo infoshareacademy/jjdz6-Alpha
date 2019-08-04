d18313333660d2bc6b2a72e2d8f5dec4e67ee766
SzymonGierszewski <szymon.gierszewski4@gmail.com>
2019-08-04 13:37:52 +0200

    add initial logger implementation
    - add slf4j and logback to pom.xml
    - add logback.xml in WWR-API resources folder
    - add initial logger in class SearchBarQueriesService

--- WWR-API/src/main/java/com/infoshare/alpha/wwr/service/SearchBarQueriesService.java
+++ WWR-API/src/main/java/com/infoshare/alpha/wwr/service/SearchBarQueriesService.java
@@ -3,6 +3,8 @@ package com.infoshare.alpha.wwr.service;
 import com.infoshare.alpha.wwr.dao.SearchBarQueryDao;
 import com.infoshare.alpha.wwr.domain.SearchBarQuery;
 import com.infoshare.alpha.wwr.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -11,11 +13,14 @@ import java.util.List;
 @RequestScoped
 public class SearchBarQueriesService {
 
+    private final Logger logger = LoggerFactory.getLogger(SearchBarQueriesService.class.getName());
+
     @Inject
     SearchBarQueryDao searchBarQueryDao;
 
     public SearchBarQuery getById(int id) {
         if (searchBarQueryDao.findById(id).isEmpty()) {
+            logger.warn("SearchBarQuery with ID: {} has not been found. ResourceNotFoundException will be thrown.", id);
             throw new ResourceNotFoundException("Query with ID " + id + " not found");
         } else {
             return searchBarQueryDao.findById(id).get();
