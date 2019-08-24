884b0950dbae90eb48d2ad034699c3095285cfd1
pkowerzanow <piotr.kowerzanow@gmail.com>
2019-08-24 22:22:00 +0200

    add branch : feature/ŁukaszKrokowski/JZ6AL-34-login-users

---
+++ WWR-APP/src/main/java/com/infoshare/alpha/wwr/servlet/AdminPanelServlet.java
@@ -0,0 +1,22 @@
+package com.infoshare.alpha.wwr.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "AdminPanelServlet", urlPatterns = {"/admin-panel"})
+public class AdminPanelServlet extends BaseWwrServlet {
+
+    private static final String ADMIN_PANEL_TEMPLATE_PATH = "/admin-panel.ftlh";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        Map<String, Object> model = new HashMap<>();
+        renderView(model, ADMIN_PANEL_TEMPLATE_PATH);
+    }
+}
