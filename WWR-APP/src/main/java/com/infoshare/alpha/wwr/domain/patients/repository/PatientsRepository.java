21641f5141644ea74f1081aafa376df6c04e8854
pkowerzanow <piotr.kowerzanow@gmail.com>
2019-08-24 16:51:04 +0200

    integration hibernate app and module app

--- WWR-APP/src/main/java/com/infoshare/alpha/wwr/domain/patients/repository/PatientsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.infoshare.alpha.wwr.domain.patients.repository;
-
-import com.infoshare.alpha.wwr.domain.patients.entity.Patients;
-
-public interface PatientsRepository {
-
-    void add(Patients patients);
-}
--- WWR-APP/src/main/webapp/js/find-facilities-by-patient.js
+++
@@ -1,6 +0,0 @@
-
-    $(".facility").on("click", function () {
-        $(this).find(".facility-details").toggle();
-    }).on("click", "ul", function (e) {
-        e.stopPropagation();
-    });
