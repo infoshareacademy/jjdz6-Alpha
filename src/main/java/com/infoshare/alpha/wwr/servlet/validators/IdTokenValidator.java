873f9bc93ecc3fb29d0edf079655d243c53f6eb2
kroko22 <kroko79@gmail.com>
2019-08-24 14:05:28 +0200

    to do filter and validator

--- src/main/java/com/infoshare/alpha/wwr/servlet/validators/IdTokenValidator.java
+++ src/main/java/com/infoshare/alpha/wwr/servlet/validators/IdTokenValidator.java
@@ -26,7 +26,6 @@ public class IdTokenValidator {
         if (tokenIsValid) {
             GoogleIdToken.Payload payload = token.getPayload();
 
-
             if (!GOOGLE_CLIENT_ID.equals(payload.getAudience())) {
                 throw new IllegalArgumentException("Audience mismatch");
             } else if (!GOOGLE_CLIENT_ID.equals(payload.getAuthorizedParty())) {
